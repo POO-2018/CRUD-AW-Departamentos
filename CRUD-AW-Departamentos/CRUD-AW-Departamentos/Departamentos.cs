7c44b67440e3492fa3afc95b18335036c473fe9a
Hector Sabillon <h.hsabillon@gmail.com>
2018-10-22 10:35:12 -0600

    Método de inserción de Departamento

--- CRUD-AW-Departamentos/CRUD-AW-Departamentos/Departamentos.cs
+++ CRUD-AW-Departamentos/CRUD-AW-Departamentos/Departamentos.cs
@@ -71,5 +71,45 @@ namespace CRUD_AW_Departamentos
                 conn.Close();
             }
         }
+
+        public bool Insertar(Departamento departamento)
+        {
+            // Creación del query de inserción
+            sql = @"INSERT INTO HumanResources.Department(Name, GroupName, ModifiedDate)
+                    VALUES (@name, @groupName, @modifiedDate";
+
+            // Crear el comando
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                // Establecer la conexión
+                conn.Open();
+
+                using (cmd)
+                {
+                    // Cambiando los paránetros del query de inserción
+                    // por los valores a insertar
+                    cmd.Parameters.Add("@name", SqlDbType.NVarChar).Value = departamento.name;
+                    cmd.Parameters.Add("@groupName", SqlDbType.NVarChar).Value = departamento.groupName;
+                    cmd.Parameters.Add("@modifiedDate", SqlDbType.DateTime).Value = departamento.modifiedDate;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Como el query de inserción se ejecutó correctamente retornar true
+                return true;
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("Error al momento de realizar la inserción");
+                return false;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                conn.Close();
+            }
+        }
     }
 }
